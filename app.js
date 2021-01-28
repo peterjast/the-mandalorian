7c837c6254a23a49c7125ec306b7da760a0f4761
Peter Staker <peterjast@gmail.com>
2021-01-28 12:01:13 -0800

    refactored js into one file

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 'use strict'
 
-let userName = prompt('Please Enter Your Name: ');
+function getUserName(){
+    let message;
+    let userName = prompt('Please Enter Your Name: ');
+    if (userName != false && userName != null) {
+        message = 'Welcome ' + userName + '!'; 
+    } else {
+        message = 'Welcome!!!';
+    }
+    document.write(message);   
+}        
 
-document.write('Welcome ' + userName + '!');
+let byeMessage;
 
+function getTimeOfDay() {
+    let today = new Date();
+    let hourNow = today.getHours();
+    let timeOfDay;
+    if (hourNow >= 18) {
+        timeOfDay = 'evening';
+    } else if (hourNow >= 12) {
+        timeOfDay = 'afternoon';
+    } else if (hourNow >= 0) {
+        timeOfDay = 'morning';
+    } else {
+        timeOfDay = 'day';
+    }
+    byeMessage = ('Have a wonderful ' + timeOfDay + '!');
+}
 
-
+function reveal() { 
+    let message;
+    let revealName = confirm('Are you sure you want to know?');
+    if (revealName === true) {
+        message = '<h2>Din Djarin!</h2><br><img src="https://cdn.flickeringmyth.com/wp-content/uploads/2020/12/The-Mandalorian-Din-Djarin-Pedro-Pascal.jpg">'
+    } else {
+        message = 'No worries! Come back if you change your mind.'};
+    getTimeOfDay();    
+    document.getElementById("realName").innerHTML = message; 
+    document.getElementById("bye").innerHTML = byeMessage;   
+} 
--- app2.js
+++
@@ -1,3 +0,0 @@
-function reveal() { 
-    confirm("Din Djarin"); 
-} 
